cc4449b2b48a631e1112d52e9393d962c379701a
CharlesCoulais <charles@coulais.email>
2025-11-16 22:38:47 +0100

    👷 change output file path

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import terser from "@rollup/plugin-terser";
 export default {
 	input: 'src/main.js',
 	output: {
-		file: 'bundle.js',
+		file: 'assets/scripts/app.js',
 		format: 'iife',
     compact: true,
     minifyInternalExports: true,
