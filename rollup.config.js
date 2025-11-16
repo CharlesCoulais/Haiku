9e5a212c42ab29843a675a6a431b1c1b12d69ac1
CharlesCoulais <charles@coulais.email>
2025-11-16 22:42:34 +0100

    🎨 Re-organize files to isolate extension app

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import terser from "@rollup/plugin-terser";
 export default {
 	input: 'src/main.js',
 	output: {
-		file: 'assets/scripts/app.js',
+		file: 'app/assets/scripts/app.js',
 		format: 'iife',
     compact: true,
     minifyInternalExports: true,
