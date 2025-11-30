f832b09d3a91c933e32bc96a2673ec996495ca7a
CharlesCoulais <charles@coulais.email>
2025-11-30 02:17:15 +0100

    🚚 move background.js to be roll(ed)up

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,32 @@
 import terser from "@rollup/plugin-terser";
 import html from "rollup-plugin-html";
 
-export default {
-	input: 'src/main.js',
-	plugins: [
-		html({
-			include: "**/*.html"
-		}),
-	],
-	output: {
-		file: 'app/assets/scripts/app.js',
-		format: 'iife',
-    compact: true,
-    minifyInternalExports: true,
-		plugins: [terser()],
-		sourcemap: true,
-	}
-};
+export default [
+	{
+		input: 'src/main.js',
+		plugins: [
+			html({
+				include: "**/*.html",
+			}),
+		],
+		output: {
+			file: 'app/assets/scripts/app.js',
+			format: 'iife',
+			compact: true,
+			minifyInternalExports: true,
+			plugins: [terser()],
+			sourcemap: true,
+		},
+	},
+	{
+		input: 'src/background.js',
+		output: {
+			file: 'app/assets/scripts/background.js',
+			format: 'iife',
+			compact: true,
+			minifyInternalExports: true,
+			plugins: [terser()],
+			sourcemap: true,
+		},
+	},
+];
