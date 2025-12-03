a1757d687f05f083243322cc5f162ffa98cff529
CharlesCoulais <charles@coulais.email>
2025-12-03 15:10:27 +0100

    ➕ use yalc to local dev

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import terser from "@rollup/plugin-terser";
 import html from "rollup-plugin-html";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default [
 	{
 		input: 'src/main.js',
 		plugins: [
+			nodeResolve(),
 			html({
 				include: "**/*.html",
 			}),
